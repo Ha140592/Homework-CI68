fdea5d894e377ad695b426644d165af2790dc6e7
Nguyen Ha <ngocha.mac@gmail.com>
2021-10-13 02:47:22 +0900

    update btvn buoi 3

--- homework3/ex2/app.js
+++ homework3/ex2/app.js
@@ -5,18 +5,14 @@ searchBtn.addEventListener("click", searchBook);
 async function searchBook() {
     const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchKey.value}&key=AIzaSyBZU5-EDorDiQ-2ni3Bf7v0kuHj16AW_BQ`);
     const res = await response.json()
-    console.log(res);
-    console.log(typeof(res));
-    for (var i = 0; i < res.items.length; i++) {
-        var item = res.items[i];
-        console.log(item.volumeInfo.title +item.volumeInfo.authors + '' +item.volumeInfo.imageLinks.thumbnail) ;
+    for (let i = 0; i < res.items.length; i++) {
+        let item = res.items[i];
         searchResult.insertAdjacentHTML('afterBegin', 
         `<div class="book">
-        <img src = ${item.volumeInfo.imageLinks.thumbnail} alt="image">
+        <img id="img" src = ${item.volumeInfo.imageLinks.thumbnail} alt="image">
         <p id="title"><b>${item.volumeInfo.title}</b></p>
         <p id="author">${item.volumeInfo.authors}</p>
         <div id="detail-btn">Detail</div>
         </div>`);
-      }
-    
+      }   
 }
