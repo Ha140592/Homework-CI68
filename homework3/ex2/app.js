2300bcb307e517de7d9ea97db6a89430187b971a
Nguyen Ha <ngocha.mac@gmail.com>
2021-10-13 03:09:02 +0900

    update btvn buoi 3

--- homework3/ex2/app.js
+++ homework3/ex2/app.js
@@ -2,17 +2,25 @@ let searchKey = document.getElementById("search-input");
 let searchBtn = document.getElementById("search-btn");
 let searchResult =  document.getElementById("search-result");
 searchBtn.addEventListener("click", searchBook);
-async function searchBook() {
-    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchKey.value}&key=AIzaSyBZU5-EDorDiQ-2ni3Bf7v0kuHj16AW_BQ`);
-    const res = await response.json()
-    for (let i = 0; i < res.items.length; i++) {
-        let item = res.items[i];
-        searchResult.insertAdjacentHTML('afterBegin', 
-        `<div class="book">
-        <img id="img" src = ${item.volumeInfo.imageLinks.thumbnail} alt="image">
-        <p id="title"><b>${item.volumeInfo.title}</b></p>
-        <p id="author">${item.volumeInfo.authors}</p>
-        <div id="detail-btn">Detail</div>
-        </div>`);
-      }   
+async function search(){
+  const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchKey.value}&key=AIzaSyBZU5-EDorDiQ-2ni3Bf7v0kuHj16AW_BQ`);
+  const res = await response.json()
+  for (let i = 0; i < res.items.length; i++) {
+      let item = res.items[i];
+      searchResult.insertAdjacentHTML('afterBegin', 
+      `<div class="book">
+      <img id="img" src = ${item.volumeInfo.imageLinks.thumbnail} alt="image">
+      <p id="title"><b>${item.volumeInfo.title}</b></p>
+      <p id="author">${item.volumeInfo.authors}</p>
+      <div id="detail-btn">Detail</div>
+      </div>`);
+    }
+  }
+function searchBook() {
+  if(searchResult.innerHTML != "") {
+    searchResult.innerHTML = ""
+    search();
+  } else {
+    search();
+    } 
 }
