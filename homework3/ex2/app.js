8205165980bbc162ac657c49d44324831e663430
Nguyen Ha <ngocha.mac@gmail.com>
2021-10-13 14:25:22 +0900

    update btvn buoi 3

--- homework3/ex2/app.js
+++ homework3/ex2/app.js
@@ -2,6 +2,11 @@ let searchKey = document.getElementById("search-input");
 let searchBtn = document.getElementById("search-btn");
 let searchResult =  document.getElementById("search-result");
 searchBtn.addEventListener("click", searchBook);
+document.addEventListener("keyup", function(event) {
+  if (event.code === 'Enter') {
+    searchBook();
+  }
+});
 async function search(){
   const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchKey.value}&key=AIzaSyBZU5-EDorDiQ-2ni3Bf7v0kuHj16AW_BQ`);
   const res = await response.json()
@@ -14,13 +19,13 @@ async function search(){
       <p id="author">${item.volumeInfo.authors}</p>
       <div id="detail-btn">Detail</div>
       </div>`);
-    }
   }
+}
 function searchBook() {
   if(searchResult.innerHTML != "") {
-    searchResult.innerHTML = ""
+    searchResult.innerHTML = "";
     search();
   } else {
     search();
-    } 
+  } 
 }
